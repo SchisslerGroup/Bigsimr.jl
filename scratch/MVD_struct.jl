6e30144abff1f850d4044d74c4d74d2d4d216d84
Alex Knudson <aknudson@nevada.unr.edu>
2020-08-01 14:58:13 -0700

    removed old scratch files

--- scratch/MVD_struct.jl
+++ scratch/MVD_struct.jl
@@ -1,31 +1,30 @@
-using Distributions
 using MvSim
+using Distributions
 
 struct MixedMultivariateDistribution{T<:Real}
     margins::Tuple{Vararg{UnivariateDistribution}}
-    rho::AbstractArray{T,2}
-    cortype::String
+    rho::Array{T,2}
+    cortype::Symbol
     μ::NTuple{N,T} where {N}
     σ::NTuple{N,T} where {N}
 end
 
 function MixedMultivariateDistribution(
     D::Tuple{Vararg{UnivariateDistribution}},
-    R::AbstractArray,
-    cortype::String,
+    R::Matrix,
+    cortype::Symbol,
 )
     μ = mean.(D)
     σ = std.(D)
 
-    cortype = lowercase(cortype)
-    @assert cortype ∈ ["pearson", "spearman", "kendall"]
+    @assert cortype ∈ keys(CorrelationTypes)
 
     MixedMultivariateDistribution(D, R, cortype, μ, σ)
 end
 
 margins = (Beta(2, 3), Normal(5, 2.2), Binomial(2, 0.2), Binomial(20, 0.2))
 r = rcor(4)
-MVD = MixedMultivariateDistribution(margins, r, "spearman")
+MVD = MixedMultivariateDistribution(margins, r, :S)
 MVD.cortype
 MVD.μ
 MVD.σ
