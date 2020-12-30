d7801ca82b10f54988b8ba3a6ceeae6f3058e858
Alex Knudson <aknudson@nevada.unr.edu>
2020-12-29 21:23:37 -0800

    bug: cor_convert was not properly set up for matrices

--- src/Correlation/utils.jl
+++ src/Correlation/utils.jl
@@ -12,7 +12,7 @@ cor(x,    ::Type{Spearman}) = corspearman(x)
 cor(x, y, ::Type{Spearman}) = corspearman(x, y)
 cor(x,    ::Type{Kendall})  = corkendall(x)
 cor(x, y, ::Type{Kendall})  = corkendall(x, y)
-# cor(x::AbstractVector, ::Correlation) = cor(x)
+
 
 """
     cor_convert(ρ::Real, from::Correlation, to::Correlation)
@@ -21,14 +21,14 @@ Convert from one type of correlation matrix to another. The possible correlation
 types are Pearson, Spearman, or Kendall.
 """
 function cor_convert end
-cor_convert(ρ, from::Type{C}, to::Type{C}) where {C<:Correlation} = ρ
-cor_convert(ρ, from::Type{Pearson},  to::Type{Spearman}) = (6 / π) * asin(ρ / 2)
-cor_convert(ρ, from::Type{Pearson},  to::Type{Kendall})  = (2 / π) * asin(ρ)
-cor_convert(ρ, from::Type{Spearman}, to::Type{Pearson})  = 2 * sin(ρ * π / 6)
-cor_convert(ρ, from::Type{Spearman}, to::Type{Kendall})  = (2 / π) * asin(2 * sin(ρ * π / 6))
-cor_convert(ρ, from::Type{Kendall},  to::Type{Pearson})  = sin(ρ * π / 2)
-cor_convert(ρ, from::Type{Kendall},  to::Type{Spearman}) = (6 / π) * asin(sin(ρ * π / 2) / 2)
-cor_convert(R::AbstractMatrix, from::Type{Correlation}, to::Type{Correlation}) = cor_convert.(copy(R), from, to)
+cor_convert(ρ::AbstractFloat, from::Type{C}, to::Type{C}) where {C<:Correlation} = ρ
+cor_convert(ρ::AbstractFloat, from::Type{Pearson},  to::Type{Spearman}) = (6 / π) * asin(ρ / 2)
+cor_convert(ρ::AbstractFloat, from::Type{Pearson},  to::Type{Kendall})  = (2 / π) * asin(ρ)
+cor_convert(ρ::AbstractFloat, from::Type{Spearman}, to::Type{Pearson})  = 2 * sin(ρ * π / 6)
+cor_convert(ρ::AbstractFloat, from::Type{Spearman}, to::Type{Kendall})  = (2 / π) * asin(2 * sin(ρ * π / 6))
+cor_convert(ρ::AbstractFloat, from::Type{Kendall},  to::Type{Pearson})  = sin(ρ * π / 2)
+cor_convert(ρ::AbstractFloat, from::Type{Kendall},  to::Type{Spearman}) = (6 / π) * asin(sin(ρ * π / 2) / 2)
+cor_convert(R::Matrix{<:AbstractFloat}, from::Type{<:Correlation}, to::Type{<:Correlation}) = cor_convert.(copy(R), from, to)
 
 
 function cor_constrain(C::AbstractMatrix)
