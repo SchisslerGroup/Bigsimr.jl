eeb9e990e0e067771cbefdfa19f81de64605a556
Alex Knudson <aknudson@nevada.unr.edu>
2021-02-14 10:14:19 -0800

    tests: simplified correlation tests
    tests now use more loops to reduce the number of lines. Can be expanded later if more correlation types are added

--- test/Correlation.jl
+++ test/Correlation.jl
@@ -4,6 +4,7 @@ using LinearAlgebra
 using Distributions
 
 @testset "Correlation Functions" begin
+
     r_negdef = [
         1.00 0.82 0.56 0.44
         0.82 1.00 0.28 0.85
@@ -142,124 +143,62 @@ end
 
 @testset "Correlation Utilities" begin
 
+    cor_types = (Pearson, Spearman, Kendall)
+
     @testset "Correlation calculation" begin
+        # Must work for any floating point matrix or vector pair
         test_types = [Float64, Float32, Float16]
         for T in test_types
             A = rand(T, 200, 4)
             x, y = rand(T, 100), rand(T, 100)
 
-            @test_nowarn cor(A, Pearson)
-            @test_nowarn cor(A, Spearman)
-            @test_nowarn cor(A, Kendall)
+            for C in cor_types
+                @test_nowarn cor(A, C)
+                @test_nowarn cor(x, y, C)
 
-            @test_nowarn cor(x, y, Pearson)
-            @test_nowarn cor(x, y, Spearman)
-            @test_nowarn cor(x, y, Kendall)
+                @test_nowarn cor_fast(A, C)
+            end
         end
 
-        test_types = [Float64, Float32, Float16]
-        for T in test_types
-            A = rand(T, 200, 4)
-
-            @test_nowarn cor_fast(A, Pearson)
-            @test_nowarn cor_fast(A, Spearman)
-            @test_nowarn cor_fast(A, Kendall)
-        end
     end
 
     @testset "Correlation to correlation conversion" begin
-        rs = cor_randPSD(4)
-        rk = cor_randPSD(4)
-        rp = cor_randPSD(4)
-        rpp = cor_convert(rp, Pearson,  Pearson)
-        rps = cor_convert(rp, Pearson,  Spearman)
-        rpk = cor_convert(rp, Pearson,  Kendall)
-        rsp = cor_convert(rs, Spearman, Pearson)
-        rss = cor_convert(rs, Spearman, Spearman)
-        rsk = cor_convert(rs, Spearman, Kendall)
-        rkp = cor_convert(rk, Kendall,  Pearson)
-        rks = cor_convert(rk, Kendall,  Spearman)
-        rkk = cor_convert(rk, Kendall,  Kendall)
 
-        @test rs == rss
-        @test rk == rkk
-        @test rp == rpp
-
-        @test cor_convert(0.0, Spearman, Kendall)  == 0.0
-        @test cor_convert(0.0, Spearman, Pearson)  == 0.0
-        @test cor_convert(0.0, Kendall,  Spearman) == 0.0
-        @test cor_convert(0.0, Kendall,  Kendall)  == 0.0
-        @test cor_convert(0.0, Kendall,  Pearson)  == 0.0
-        @test cor_convert(0.0, Pearson,  Spearman) == 0.0
-        @test cor_convert(0.0, Pearson,  Kendall)  == 0.0
-        @test cor_convert(0.0, Pearson,  Pearson)  == 0.0
-        @test cor_convert(0.0, Spearman, Spearman) == 0.0
-
-        @test cor_convert(1.0, Spearman, Kendall)  ≤ 1.0
-        @test cor_convert(1.0, Spearman, Pearson)  ≤ 1.0
-        @test cor_convert(1.0, Kendall,  Spearman) ≤ 1.0
-        @test cor_convert(1.0, Kendall,  Kendall)  ≤ 1.0
-        @test cor_convert(1.0, Kendall,  Pearson)  ≤ 1.0
-        @test cor_convert(1.0, Pearson,  Spearman) ≤ 1.0
-        @test cor_convert(1.0, Pearson,  Kendall)  ≤ 1.0
-        @test cor_convert(1.0, Pearson,  Pearson)  ≤ 1.0
-        @test cor_convert(1.0, Spearman, Spearman) ≤ 1.0
-
-        @test cor_convert(1.0, Spearman, Kendall)  ≈ 1.0
-        @test cor_convert(1.0, Spearman, Pearson)  ≈ 1.0
-        @test cor_convert(1.0, Kendall,  Spearman) ≈ 1.0
-        @test cor_convert(1.0, Kendall,  Kendall)  ≈ 1.0
-        @test cor_convert(1.0, Kendall,  Pearson)  ≈ 1.0
-        @test cor_convert(1.0, Pearson,  Spearman) ≈ 1.0
-        @test cor_convert(1.0, Pearson,  Kendall)  ≈ 1.0
-        @test cor_convert(1.0, Pearson,  Pearson)  ≈ 1.0
-        @test cor_convert(1.0, Spearman, Spearman) ≈ 1.0
-
-        @test cor_convert(-1.0, Spearman, Kendall)  ≥ -1.0
-        @test cor_convert(-1.0, Spearman, Pearson)  ≥ -1.0
-        @test cor_convert(-1.0, Kendall,  Spearman) ≥ -1.0
-        @test cor_convert(-1.0, Kendall,  Kendall)  ≥ -1.0
-        @test cor_convert(-1.0, Kendall,  Pearson)  ≥ -1.0
-        @test cor_convert(-1.0, Pearson,  Spearman) ≥ -1.0
-        @test cor_convert(-1.0, Pearson,  Kendall)  ≥ -1.0
-        @test cor_convert(-1.0, Pearson,  Pearson)  ≥ -1.0
-        @test cor_convert(-1.0, Spearman, Spearman) ≥ -1.0
-
-        @test cor_convert(-1.0, Spearman, Kendall)  ≈ -1.0
-        @test cor_convert(-1.0, Spearman, Pearson)  ≈ -1.0
-        @test cor_convert(-1.0, Kendall,  Spearman) ≈ -1.0
-        @test cor_convert(-1.0, Kendall,  Kendall)  ≈ -1.0
-        @test cor_convert(-1.0, Kendall,  Pearson)  ≈ -1.0
-        @test cor_convert(-1.0, Pearson,  Spearman) ≈ -1.0
-        @test cor_convert(-1.0, Pearson,  Kendall)  ≈ -1.0
-        @test cor_convert(-1.0, Pearson,  Pearson)  ≈ -1.0
-        @test cor_convert(-1.0, Spearman, Spearman) ≈ -1.0
-
-        # Must work for each type. Must respect the input eltype
-        test_types = [Float64, Float32, Float16, Rational]
-        for T in test_types
-            @test_nowarn cor_convert(T(0.5), Pearson,  Pearson)
-            @test_nowarn cor_convert(T(0.5), Pearson,  Spearman)
-            @test_nowarn cor_convert(T(0.5), Pearson,  Kendall)
-            @test_nowarn cor_convert(T(0.5), Spearman, Pearson)
-            @test_nowarn cor_convert(T(0.5), Spearman, Spearman)
-            @test_nowarn cor_convert(T(0.5), Spearman, Kendall)
-            @test_nowarn cor_convert(T(0.5), Kendall,  Pearson)
-            @test_nowarn cor_convert(T(0.5), Kendall,  Spearman)
-            @test_nowarn cor_convert(T(0.5), Kendall,  Kendall)
+        # Converting type A -> A must result in the same matrix
+        for C in cor_types
+            r = cor_randPD(4)
+            @test r == cor_convert(r, C, C)
         end
 
+        # Must map (-1, 0, 1) onto itself within numerical error
+        for C1 in cor_types
+            for C2 in cor_types
+                @test cor_convert( 0.0, C1, C2)  == 0.0
+                @test cor_convert( 1.0, C1, C2)   ≤ 1.0
+                @test cor_convert( 1.0, C1, C2)   ≈ 1.0
+                @test cor_convert(-1.0, C1, C2)  ≥ -1.0
+                @test cor_convert(-1.0, C1, C2)  ≈ -1.0
+            end
+        end
+
+        # Must work for each type
+        test_types = [Float64, Float32, Float16, Rational]
+        for T in test_types
+            for C1 in cor_types
+                for C2 in cor_types
+                    @test_nowarn cor_convert(T(0.5), C1,  C2)
+                end
+            end
+        end
+
+        # Must respect these input eltypes
         test_types = [Float64, Float32, Float16]
         for T in test_types
-            @test eltype(cor_convert(T(0.5), Pearson,  Pearson))  === T
-            @test eltype(cor_convert(T(0.5), Pearson,  Spearman)) === T
-            @test eltype(cor_convert(T(0.5), Pearson,  Kendall))  === T
-            @test eltype(cor_convert(T(0.5), Spearman, Pearson))  === T
-            @test eltype(cor_convert(T(0.5), Spearman, Spearman)) === T
-            @test eltype(cor_convert(T(0.5), Spearman, Kendall))  === T
-            @test eltype(cor_convert(T(0.5), Kendall,  Pearson))  === T
-            @test eltype(cor_convert(T(0.5), Kendall,  Spearman)) === T
-            @test eltype(cor_convert(T(0.5), Kendall,  Kendall))  === T
+            for C1 in cor_types
+                for C2 in cor_types
+                    @test eltype(cor_convert(T(0.5), C1,  C2))  === T
+                end
+            end
         end
     end
 
@@ -283,19 +222,18 @@ end
 
     @testset "Correlation Bounds" begin
         A, B = NegativeBinomial(20, 0.2), LogNormal(3, 1)
+        dist_types = (A, B)
 
-        @test_nowarn cor_bounds(A, A, Pearson)
-        @test_nowarn cor_bounds(A, A, Spearman)
-        @test_nowarn cor_bounds(A, A, Kendall)
+        # Must work for any univariate distribution and correlation type
+        for D1 in dist_types
+            for D2 in dist_types
+                for C in cor_types
+                    @test_nowarn cor_bounds(D1, D2, C)
+                end
+            end
+        end
 
-        @test_nowarn cor_bounds(A, B, Pearson)
-        @test_nowarn cor_bounds(A, B, Spearman)
-        @test_nowarn cor_bounds(A, B, Kendall)
-
-        @test_nowarn cor_bounds(B, A, Pearson)
-        @test_nowarn cor_bounds(B, A, Spearman)
-        @test_nowarn cor_bounds(B, A, Kendall)
-
+        # Must work for any number with an integer representation
         test_types = [Float64, Float32, Float16, Int64, Int32, Int16]
         for T in test_types
             @test_nowarn cor_bounds(A, B, n_samples=T(10_000))
