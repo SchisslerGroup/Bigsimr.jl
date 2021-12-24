8581c14ae34cf5a2c826eac78a811f1868c841a0
Alex Knudson <aknudson@nevada.unr.edu>
2021-12-24 13:47:49 -0800

    need to set env var for GR plots to work on CI

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,11 @@ using Distributions
 
 DocMeta.setdocmeta!(Bigsimr, :DocTestSetup, :(using Bigsimr); recursive=true)
 
+# Gotta set this environment variable when using the GR run-time on CI machines.
+# This happens as examples will use Plots.jl to make plots and movies.
+# See: https://github.com/jheinen/GR.jl/issues/278
+ENV["GKSwstype"] = "100"
+
 makedocs(
     sitename = "Bigsimr.jl",
     modules  = [Bigsimr],
